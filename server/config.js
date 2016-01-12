43746caa6020c4caddcfa53a75e74f399850f6fc
Daniele Zanni <syymza@gmail.com>
2016-01-12 18:42:14 +0000

    Update config.js

--- server/config.js
+++ server/config.js
@@ -28,9 +28,6 @@ if (fs.existsSync(devEnvPath)) {
 // Express config
 exports.PORT = process.env.PORT || 5000;
 
-// General config for polling requests
-exports.POLL_INTERVAL = process.env.POLL_INTERVAL || 1000 * 60 * 1; // ms * secs * mins
-
 // Application constants
 exports.VERSION = pkg.version;
 
@@ -39,7 +36,6 @@ var CAPI_KEY = process.env.CAPI_KEY || devEnv.CAPI_KEY;
 exports.CAPI_KEY = CAPI_KEY;
 exports.SEARCH_QUERY = "lastPublishDateTime:>"; // To restrict to just news items: "genre:=\"News\" AND lastPublishDateTime:>"
 exports.MAX_RESULTS = 100;
-exports.TIME_LIMIT = process.env.TIME_LIMIT || devEnv.TIME_LIMIT || 8; // A time in past measured in hours
 exports.capiSearchPath = 'http://api.ft.com/content/search/v1?apiKey=' + CAPI_KEY;
 
 // My alerts config
